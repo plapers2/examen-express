1f63fc5218014776a4c89c33670c2403fcd5ca2a
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-03-14 10:18:17 -0500

    Se configura index y app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 import express from "express";
+import cors from "cors";
+//* Importar rutas de autos
+import autosRutas from "./src/routes/autos.route.js";
+// Instancia global
+const app = express();
+
+//* Middlewares necesarios
+app.use(cors());
+app.use(express.json());
+
+//* Uso de rutas
+app.use("/api", autosRutas);
+
+export default app;
