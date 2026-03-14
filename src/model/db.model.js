17f6b99c2b89b660b5b20750fcce7d45f8479959
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-03-14 10:43:48 -0500

    Se configura para subir a render

--- src/model/db.model.js
+++ src/model/db.model.js
@@ -1,11 +1,14 @@
 import MySQL from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 let cnx;
 try {
   cnx = MySQL.createPool({
-    host: "localhost",
-    user: "root",
-    database: "autos_examen",
-    port: "3306",
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT_BD,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
